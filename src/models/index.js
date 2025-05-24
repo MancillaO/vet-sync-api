dbb6705c616128a22b55fc8561424b9e7d935c29
MancillaO <edgarmncllav04@gmail.com>
2025-05-24 01:56:40 -0600

    fix supabase extraction

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 import { supabaseUrl, supabaseKey } from '../../config.js'
 
-export const supabase = () => {
-  return createClient(supabaseUrl, supabaseKey)
-}
+export const supabase = createClient(supabaseUrl, supabaseKey)
