e7b746d9acab11ed4df8ff54b6866478f0a72492
MancillaO <edgarmncllav04@gmail.com>
2025-05-25 01:31:45 -0600

    add logic to get species in different scenarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import { authenticateToken } from '../middlewares/auth.js'
 import { userRouter } from './userRouter.js'
 import { authRouter } from './authRouter.js'
 import { petRouter } from './petRouter.js'
+import { speciesRouter } from './speciesRouter.js'
 
 export const router = Router()
 
 router.use('/users', userRouter)
 router.use('/auth', authRouter)
-router.use('/pets', petRouter)
+router.use('/species', authenticateToken, speciesRouter)
+router.use('/pets', authenticateToken, petRouter)
