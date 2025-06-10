4f138479111ca209ec13e32a96914734e505e0da
MancillaO <edgarmncllav04@gmail.com>
2025-06-10 12:02:38 -0600

    fix

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ export const {
   PORT: port = '3000',
   NODE_ENV,
   logger = NODE_ENV === 'production' ? 'combined' : 'dev',
-  JWT_SECRET,
   SUPABASE_URL: supabaseUrl,
   SUPABASE_ANON_KEY: supabaseKey
 } = process.env
