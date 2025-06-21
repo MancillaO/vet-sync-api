b48802e8454e2c9c9ffd32b61348de81c7a9d0d2
MancillaO <edgarmncllav04@gmail.com>
2025-06-21 13:17:50 -0600

    delete token authentication from services enpoints

--- src/routes/servicesRouter.js
+++ src/routes/servicesRouter.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { ServiceController } from '#controllers/serviceController.js'
+import { authenticateToken } from '#middlewares/auth.js'
 
 export const servicesRouter = Router()
 
 servicesRouter.get('/', ServiceController.getAllServices)
 servicesRouter.get('/:id', ServiceController.getById)
 servicesRouter.get('/active/:active', ServiceController.getActiveServices)
+
+servicesRouter.use(authenticateToken)
 
 servicesRouter.post('/', ServiceController.addService)
 
